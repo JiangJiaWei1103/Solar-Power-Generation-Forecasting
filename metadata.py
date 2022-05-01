9b137bfde851f51604127efd0ff2042082f0c3a7
jiangjiawei1103 <waynechuang97@gmail.com>
2022-05-01 22:29:25 +0800

    first commit

---
+++ metadata.py
@@ -0,0 +1,9 @@
+"""
+Project-specific metadata for global access.
+Author: JiaWei Jiang
+"""
+# Originally provided column names with self-defined ordering
+COLS = ["ID", "Date", "Lat", "Lon", 
+        "Irradiance", "Temp", 
+        "Module", "Capacity", "Angle", "Irradiance_m", 
+        "Temp_m", "Generation"]
---
+++ paths.py
@@ -0,0 +1,10 @@
+"""
+Path definitions for global access.
+Author: JiaWei Jiang
+"""
+# Path of configuration files
+CONFIG_PATH = "./config"
+# Path of raw data
+RAW_DATA_PATH = "./data/raw"
+# Path of output buffer
+DUMP_PATH = "./output"
